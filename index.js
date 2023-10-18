e0e3e0f0cb42ca80e991e14937bb1c76388959d3
Eric Pogue <epogue@epogue.com>
2023-10-18 13:45:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + '/client'))
+
+
+app.get('/test', function(request, response) {
+	response.type('text/plain')
+	response.send('Node.js and Express running on port='+port)
+})
+
+// Load your JSON data
+const favoritePlaces = require('./FavoritePlaces.json');
+
+// Create a route that serves the JSON data
+app.get('/api/favorite-places', (req, res) => {
+  res.json(favoritePlaces);
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
